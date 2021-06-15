7f292353f94abdc676e8332189baa699b3e1c416
wassim barhoumi <wassimix14@gmail.com>
2021-06-15 22:02:55 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,8 @@
+import React from "react"
+const Address = () => {
+        return ( 
+            <h2 style={{ color: "green", fontSize: 80 ,fontFamily:"Times New Roman" }}> El menzah 9b </h2>
+        );
+    }
+
+export default Address ;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,8 @@
+import React from "react"
+const Photo = () => {
+        return ( 
+            <img src="/PofilePhoto.jpg" className="Photos" alt="ProfilePhoto" />
+        );
+    }
+
+export default Photo ;
